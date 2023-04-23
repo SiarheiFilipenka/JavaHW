3549c6a94b872d60f7bbcc836e5ab72f1f20b3af
SiarheiFilipenka <sergeyfilippenko89@gmail.com>
2023-04-23 00:23:31 -0500

    Class 12 HW

---
+++ src/hw9_class12/Task2.java
@@ -0,0 +1,17 @@
+package hw9_class12;
+
+import java.util.Scanner;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the word:");
+        String word = input.next();
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            System.out.print(word.charAt(i));
+        }
+    }
+}
